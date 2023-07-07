77c54c9cdcae9fd513e11965009938e35df56856
HeineCantor <carmine.palmese@gmail.com>
2023-07-08 00:24:56 +0200

    proto-implementation of state vector printing in dirac notation (for debugging and aesthetic purposes :>)

--- src/print_util.h
+++ src/print_util.h
@@ -3,21 +3,34 @@
 
 #include <iostream>
 #include <cuComplex.h>
+#include <bitset>
 
 using namespace std;
 
-void printSimulationDetails(int numQubits, int threadNumber, int blockNumber)
+void printGenericSimulationDetails(int numQubits)
 {
     cout << "Qubit number: " << numQubits << endl;
     cout << "States number: " << (1 << numQubits) << endl;
 
     cout << endl;
+}
+
+void printSingleQubitSimulationDetails(int numQubits, int threadNumber, int blockNumber)
+{
+    printGenericSimulationDetails(numQubits);
 
     cout << "======= SIMULATION FOR ONE QUBIT GATE =======" << endl;
     cout << "Required Thread number: " << threadNumber << endl;
     cout << "Required Blocks number: " << blockNumber << endl;
 
     cout << endl;
+}
+
+void printNQubitsSimulationDetails(int numQubits)
+{
+    printGenericSimulationDetails(numQubits);
+
+
 }
 
 void printStateVector(cuDoubleComplex* vector, int vectorCount, int maxStatesToPrint = 0)
@@ -41,6 +54,26 @@ void printStateVector(cuDoubleComplex* vector, int vectorCount, int maxStatesToP
         cout << "...";
 
     cout << " ]" << endl;
+}
+
+void printQubitsState(cuDoubleComplex* vector, int qubitCount, int maxStatesToPrint = 0)
+{
+    int vectorCount = 1 << qubitCount;
+
+    if(maxStatesToPrint == 0)
+        maxStatesToPrint = vectorCount;
+
+    int statesToPrint = vectorCount < maxStatesToPrint ? vectorCount : maxStatesToPrint;
+
+    cout << "Output Qubits State:" << endl;
+
+    for(int i = 0; i < statesToPrint; i++)
+    {
+        cout << "(" << vector[i].x << "+" << vector[i].y << "i) |" << bitset<8>(i) << ">" << endl;
+    }
+
+        if(statesToPrint < vectorCount)
+        cout << "..." << endl;
 }
 
 #endif
